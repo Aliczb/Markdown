78f86f3975cf462c95c7847fb38902b7f1fd0b80
Alice <Your emailalice.boulaypaw@hotmail.fr>
2022-10-18 20:10:28 +0200

    projet Markdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Marked from 'marked';
 import './App.css';
 
 function App() {
+  // State
+  const [texte, setTexte] = useState(
+    "# `Prévisualisateur - Believemy`\n### Oh ! De la magie !\nEn réalité, pas vraiment : ceci s'appelle du markdown, et ce projet réalisé entièrement avec React permet de le transformer en *HTML* !\n\n### Cas d'utilisation\n* *italique*\n* **gras**\n* `monospace`\n* ~~barré~~\n* # h1\n* ## h2\n* ### h3\n* #### etc\n[Believemy](https://believemy.com)"
+  );
+
+  // Fonctions
+  const changeTextHandler = (event) => {
+    setTexte(event.target.value);
+  };
+
+  const markdownToHTML = () => {
+    return { __html: Marked(texte) };
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='container'>
+        <div className='gauche'>
+          <textarea
+            rows='30'
+            value={texte}
+            onChange={(e) => changeTextHandler(e)}
+          ></textarea>
+        </div>
+
+        <div className='droite'>
+          <div dangerouslySetInnerHTML={markdownToHTML()}></div>
+        </div>
+      </div>
     </div>
   );
 }
